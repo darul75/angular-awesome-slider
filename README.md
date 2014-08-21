cbeb13768059eb81f6de1a9d6bc9d18a5f0fa966
Jacob Rief <jacob.rief@gmail.com>
2014-08-21 14:38:07 +0200

    Template can be overriden in customers html

--- README.md
+++ README.md
@@ -41,7 +41,16 @@ to the list above, you should add:
 
 ```html
 <script type="text/javascript" src="ng-slider.min.js"></script>
+<script type="text/javascript" src="ng-slider.tmpl.js"></script>
 ```
+in case you want to use your own template, omit the last line and instead add some template code
+to your project:
+```html
+<script type="text/ng-template" id="aw-select.tmpl.html">
+    ....
+</script>
+``
+
 
 Then, inject `ngSlider` in your application module:
 
--- src/ng-slider.js
+++ src/ng-slider.js
@@ -9,22 +9,7 @@
 				require: '?ngModel',
 				scope: { options:'=' },
 				priority: 1,
-				template:
-					'<span ng-class="mainSliderClass" id="{{sliderTmplId}}">' +
-						'<table><tr><td>' +
-							'<div class="jslider-bg">' +
-								'<i class="l"></i><i class="f"></i><i class="r"></i>' +
-								// '<i class="v"></i>' +
-							'</div>' +
-							'<div class="jslider-pointer"></div>' +
-							'<div class="jslider-pointer jslider-pointer-to"></div>' +
-							'<div class="jslider-label"><span ng-bind-html="from"></span></div>' +
-							'<div class="jslider-label jslider-label-to"><span ng-bind-html="to"></span>{{options.dimension}}</div>' +
-							'<div class="jslider-value"><span></span>{{options.dimension}}</div>' +
-							'<div class="jslider-value jslider-value-to"><span></span>{{options.dimension}}</div>' +
-							'<div class="jslider-scale" id="{{sliderScaleDivTmplId}}"></div>' +
-						'</td></tr></table>' +
-					'</span>',
+				templateUrl: 'ng-slider/slider-bar.tmpl.html',
 				link : function(scope, element, attrs, ngModel) {					
 					if(!ngModel) return; // do nothing if no ng-model							
 
